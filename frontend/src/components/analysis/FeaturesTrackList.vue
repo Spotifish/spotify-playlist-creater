26fc69b8cf9feec7eb947139f2af193c7352850d
Paul Orlob <orlobpaul@gmail.com>
2020-08-09 14:33:52 +0200

    Fixed duplicates in list

--- frontend/src/components/analysis/FeaturesTrackList.vue
+++ frontend/src/components/analysis/FeaturesTrackList.vue
@@ -11,11 +11,11 @@
         <th>Track</th>
         <th id="select">
           <Multiselect
-              v-model="selectedFeature"
-              :options="features"
-              :show-labels="false"
-              :searchable="false"
-              :allow-empty="false">
+            v-model="selectedFeature"
+            :options="features"
+            :show-labels="false"
+            :searchable="false"
+            :allow-empty="false">
           </Multiselect>
         </th>
       </tr>
@@ -32,118 +32,129 @@
 </template>
 
 <script>
-import {mapGetters} from "vuex";
-import Multiselect from "vue-multiselect"
+  import {mapGetters} from "vuex";
+  import Multiselect from "vue-multiselect"
 
-export default {
-  name: "FeaturesTrackList",
-  components: {
-    Multiselect
-  },
-  data: function () {
-    return {
-      features: [
-        "acousticness",
-        "danceability",
-        "energy",
-        "instrumentalness",
-        "liveness",
-        "loudness",
-        "speechiness",
-        "valence",
-        "tempo"
-      ],
-      selectedFeature: "speechiness"
-    }
-  },
-  computed: {
-    ...mapGetters([
-      "getAudioFeatures",
-      "getTracks"
-    ]),
-    trackFeatures: function () {
-      if (this.getTracks.length === this.getAudioFeatures.length) {
-        return this.getTracks.map((track, index) => {
-          return {
-            track,
-            features: this.getAudioFeatures[index]
-          }
-        })
-      } else {
-        return []
+  export default {
+    name: "FeaturesTrackList",
+    components: {
+      Multiselect
+    },
+    data: function () {
+      return {
+        features: [
+          "acousticness",
+          "danceability",
+          "energy",
+          "instrumentalness",
+          "liveness",
+          "loudness",
+          "speechiness",
+          "valence",
+          "tempo"
+        ],
+        selectedFeature: "speechiness"
       }
     },
-    list: function () {
-      return this.trackFeatures
+    computed: {
+      ...mapGetters([
+        "getAudioFeatures",
+        "getTracks"
+      ]),
+      trackFeatures: function () {
+        if (this.getTracks.length === this.getAudioFeatures.length) {
+          return this.getTracks.map((track, index) => {
+            return {
+              track,
+              features: this.getAudioFeatures[index]
+            }
+          })
+        } else {
+          return []
+        }
+      },
+      list: function () {
+        let existingNames = [];
+
+        return this.trackFeatures
           .map(o => {
             return {
               name: o.track.track.name,
               value: o.features[this.selectedFeature]
             }
           })
+          .filter(o => {
+            const hasElement = existingNames.includes(o.name);
+            if (!hasElement) {
+              existingNames.push(o.name);
+              return true;
+            } else {
+              return false;
+            }
+          })
           .sort((a, b) => b.value - a.value)
+      }
     }
   }
-}
 </script>
 
 <style scoped lang="scss">
-#features-list {
-  table {
-    width: 100%;
-    border-collapse: collapse;
-    table-layout: fixed;
-  }
-
-  tr {
-    &:nth-of-type(odd) {
-      background: $color-primary-light;
+  #features-list {
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      table-layout: fixed;
     }
 
-    td:nth-of-type(2) {
-      border-left: 1px solid $color-divider;
+    tr {
+      &:nth-of-type(odd) {
+        background: $color-primary-light;
+      }
+
+      td:nth-of-type(2) {
+        border-left: 1px solid $color-divider;
+      }
     }
-  }
 
-  th {
-    background: $color-primary-dark;
-    color: white;
-    font-weight: bold;
-    white-space: nowrap;
-  }
-
-  td, th {
-    padding: 6px;
-    text-align: left;
-  }
-
-  col:nth-of-type(2) {
-    width: 30%;
-  }
-
-  &::v-deep .multiselect {
-    overflow: visible;
-
-    .multiselect__single {
-      text-overflow: ellipsis;
-      overflow: hidden;
+    th {
+      background: $color-primary-dark;
+      color: white;
+      font-weight: bold;
       white-space: nowrap;
     }
 
-    .multiselect__content {
-      position: relative;
-      overflow: auto;
+    td, th {
+      padding: 6px;
+      text-align: left;
     }
 
-    .multiselect__option--selected {
-      background: $color-accent;
-      color: white;
+    col:nth-of-type(2) {
+      width: 30%;
     }
 
-    .multiselect__option--highlight {
-      background: $color-primary-light;
-      color: black;
+    &::v-deep .multiselect {
+      overflow: visible;
+
+      .multiselect__single {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+      }
+
+      .multiselect__content {
+        position: relative;
+        overflow: auto;
+      }
+
+      .multiselect__option--selected {
+        background: $color-accent;
+        color: white;
+      }
+
+      .multiselect__option--highlight {
+        background: $color-primary-light;
+        color: black;
+      }
     }
   }
-}
 </style>
